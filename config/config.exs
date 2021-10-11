1058c67342f2299fe89f19bd653a3ec8abb8c512
James Baldwin <jwbaldwin3@aol.com>
2021-10-11 19:53:27 -0400

    inital phx.new generator commit

---
+++ config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+import Config
+
+config :remote,
+  ecto_repos: [Remote.Repo]
+
+# Configures the endpoint
+config :remote, RemoteWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [view: RemoteWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Remote.PubSub,
+  live_view: [signing_salt: "uyu8hv1j"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
